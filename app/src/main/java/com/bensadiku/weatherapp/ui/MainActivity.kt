e6732eb4db8f347dffb5fdd5df96929b0e2c02f1
bensadiku <43443348+bensadiku@users.noreply.github.com>
2019-01-31 20:17:12 +0100

    Fixed R bug not being imported

--- app/src/main/java/com/bensadiku/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/bensadiku/weatherapp/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.bensadiku.weatherapp.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
