2572f9d1dd130b97bf005d4576c12f11935458fd
bensadiku <43443348+bensadiku@users.noreply.github.com>
2019-02-05 22:41:48 +0100

    Added the implementation to fetch future weather

--- app/src/main/java/com/bensadiku/weatherapp/data/network/WeatherNetworkDataSourceImpl.kt
+++ app/src/main/java/com/bensadiku/weatherapp/data/network/WeatherNetworkDataSourceImpl.kt
@@ -4,7 +4,11 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.bensadiku.weatherapp.data.network.response.CurrentWeatherResponse
+import com.bensadiku.weatherapp.data.network.response.FutureWeatherResponse
 import com.bensadiku.weatherapp.internal.NoConnectivityException
+
+const val TAG = "CONNECTIVITY"
+const val FORECAST_DAYS_COUNT=7
 
 class WeatherNetworkDataSourceImpl(
     private val apixuWeatherApiService: ApixuWeatherApiService
@@ -23,7 +27,25 @@ class WeatherNetworkDataSourceImpl(
             _downloadedCurrentWeather.postValue(fetchCurrentWeather)
         }
         catch (e:NoConnectivityException){
-            Log.e("Connectivity","No internet connection",e)
+            Log.e(TAG,"No internet connection",e)
+        }
+    }
+
+    //to fetch future weather////
+
+    private  val _downloadedFutureWeather = MutableLiveData<FutureWeatherResponse>()
+
+    override val downloadedFutureWeather :LiveData<FutureWeatherResponse>
+    get() = _downloadedFutureWeather
+
+    override suspend fun fetchFutureWeather(location: String, languageCode: String) {
+        try{
+            val fetchFutureWeather = apixuWeatherApiService.
+                getFutureWeather(location,FORECAST_DAYS_COUNT,languageCode)
+                .await()
+            _downloadedFutureWeather.postValue(fetchFutureWeather)
+        }catch (e:NoConnectivityException){
+            Log.e(TAG,"No internet Connection",e)
         }
     }
 }
