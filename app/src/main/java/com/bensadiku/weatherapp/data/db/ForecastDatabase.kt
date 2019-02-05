b776db99bf5c9572cbd360aed49b0e12bcdf8fa0
bensadiku <43443348+bensadiku@users.noreply.github.com>
2019-02-05 23:28:53 +0100

    Added FutureWeatherEntry as entity on the database

--- app/src/main/java/com/bensadiku/weatherapp/data/db/ForecastDatabase.kt
+++ app/src/main/java/com/bensadiku/weatherapp/data/db/ForecastDatabase.kt
@@ -6,11 +6,12 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.bensadiku.weatherapp.data.db.entity.CurrentWeatherEntry
+import com.bensadiku.weatherapp.data.db.entity.FutureWeatherEntry
 import com.bensadiku.weatherapp.data.db.entity.WeatherLocation
 
 
 @Database(
-    entities = [CurrentWeatherEntry::class, WeatherLocation::class],
+    entities = [CurrentWeatherEntry::class, WeatherLocation::class,FutureWeatherEntry::class],
     version = 1
 )
 
@@ -19,6 +20,7 @@ import com.bensadiku.weatherapp.data.db.entity.WeatherLocation
 abstract class ForecastDatabase : RoomDatabase() {
     abstract fun currentWeatherDao(): CurrentWeatherDao
     abstract fun weatherLocationDao(): WeatherLocationDao
+    abstract fun futureWeatherDao():FutureWeatherDao
 
     companion object {
         @Volatile
