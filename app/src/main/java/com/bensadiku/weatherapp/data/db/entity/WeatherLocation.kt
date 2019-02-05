1fbe53a2f050c4127da9fb75d600dc95805e68f9
bensadiku <43443348+bensadiku@users.noreply.github.com>
2019-02-05 20:20:35 +0100

    Added new entity WeatherLocation

--- app/src/main/java/com/bensadiku/weatherapp/data/db/entity/WeatherLocation.kt
+++ app/src/main/java/com/bensadiku/weatherapp/data/db/entity/WeatherLocation.kt
@@ -3,14 +3,18 @@ package com.bensadiku.weatherapp.data.db.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import org.threeten.bp.Instant
+import org.threeten.bp.ZoneId
+import org.threeten.bp.ZonedDateTime
 
+const val WEATHER_LOCATION_ID = 0
 
 @Entity(tableName = "weather_location")
 data class WeatherLocation(
     val country: String,
     val lat: Double,
     @SerializedName("localtime_epoch")
-    val localtimeEpoch: Int,
+    val localtimeEpoch: Long,
     val lon: Double,
     val name: String,
     val region: String,
@@ -18,5 +22,12 @@ data class WeatherLocation(
     val tzId: String
 ) {
     @PrimaryKey(autoGenerate = false)
-    var id: Int = 0
+    var id: Int = WEATHER_LOCATION_ID
+
+    val zonedDateTime: ZonedDateTime
+    get() {
+        val instant = Instant.ofEpochSecond(localtimeEpoch)
+        val zoneId = ZoneId.of(tzId)
+        return ZonedDateTime.ofInstant(instant,zoneId)
+    }
 }
