cc0f44b06c9ca749c2154995cd7421b89bf9f30b
bensadiku <43443348+bensadiku@users.noreply.github.com>
2019-02-05 18:40:54 +0100

    Added Glide module

---
+++ app/src/main/java/com/bensadiku/weatherapp/internal/glide/ForecastAppGlideModule.kt
@@ -0,0 +1,9 @@
+package com.bensadiku.weatherapp.internal.glide
+
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+@GlideModule
+class ForecastAppGlideModule : AppGlideModule()
