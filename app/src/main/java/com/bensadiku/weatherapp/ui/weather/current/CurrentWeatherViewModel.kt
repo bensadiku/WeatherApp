9b4dd121d4101f9a750bdd4f0c3065995a9035ef
bensadiku <43443348+bensadiku@users.noreply.github.com>
2019-02-05 19:15:16 +0100

    Removed the hard coded unit system to work with the new settings fragment

--- app/src/main/java/com/bensadiku/weatherapp/ui/weather/current/CurrentWeatherViewModel.kt
+++ app/src/main/java/com/bensadiku/weatherapp/ui/weather/current/CurrentWeatherViewModel.kt
@@ -1,17 +1,19 @@
 package com.bensadiku.weatherapp.ui.weather.current
 
 import androidx.lifecycle.ViewModel;
+import com.bensadiku.weatherapp.data.provider.UnitProvider
 import com.bensadiku.weatherapp.data.repository.ForecastRepository
 import com.bensadiku.weatherapp.internal.UnitSystem
 import com.bensadiku.weatherapp.internal.lazyDeferred
 
 class CurrentWeatherViewModel(
-    private val forecastRepository: ForecastRepository
+    private val forecastRepository: ForecastRepository,
+    unitProvider: UnitProvider
 ) : ViewModel() {
-    private val unitSystem = UnitSystem.METRIC//get from settings
+    private val unitSystem = unitProvider.getUnitSystem()
 
-    val isMetric :Boolean
-    get() = unitSystem == UnitSystem.METRIC
+    val isMetric: Boolean
+        get() = unitSystem == UnitSystem.METRIC
 
     val weather by lazyDeferred() {
         forecastRepository.getCurrentWeather(isMetric)
--- app/src/main/java/com/bensadiku/weatherapp/ui/weather/current/CurrentWeatherViewModelFactory.kt
+++ app/src/main/java/com/bensadiku/weatherapp/ui/weather/current/CurrentWeatherViewModelFactory.kt
@@ -2,14 +2,16 @@ package com.bensadiku.weatherapp.ui.weather.current
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.bensadiku.weatherapp.data.provider.UnitProvider
 import com.bensadiku.weatherapp.data.repository.ForecastRepository
 
 @Suppress("UNCHECKED_CAST")
 class CurrentWeatherViewModelFactory (
-    private val forecastRepository: ForecastRepository
+    private val forecastRepository: ForecastRepository,
+    private val unitProvider: UnitProvider
 ):ViewModelProvider.NewInstanceFactory(){
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return CurrentWeatherViewModel(forecastRepository)as T
+        return CurrentWeatherViewModel(forecastRepository,unitProvider)as T
     }
 }
