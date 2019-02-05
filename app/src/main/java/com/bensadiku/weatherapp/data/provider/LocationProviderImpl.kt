1a6c26a7914407dbfcecd7f1be4dbe3595bf6f31
bensadiku <43443348+bensadiku@users.noreply.github.com>
2019-02-05 20:39:46 +0100

    Added temporary hardcoded implementation

--- app/src/main/java/com/bensadiku/weatherapp/data/provider/LocationProviderImpl.kt
+++ app/src/main/java/com/bensadiku/weatherapp/data/provider/LocationProviderImpl.kt
@@ -4,10 +4,10 @@ import com.bensadiku.weatherapp.data.db.entity.WeatherLocation
 
 class LocationProviderImpl : LocationProvider {
     override suspend fun hasLocationChanged(lastWeatherLocation: WeatherLocation): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return true
     }
 
     override suspend fun getPreferredLocationString(): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return "Los Angeles"
     }
 }
