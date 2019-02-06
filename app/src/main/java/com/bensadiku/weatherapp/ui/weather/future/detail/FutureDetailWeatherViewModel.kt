f43477661d77467c149a2204af671b621fb735ad
bensadiku <43443348+bensadiku@users.noreply.github.com>
2019-02-06 01:02:28 +0100

    Created the view model for the future detail weather

--- app/src/main/java/com/bensadiku/weatherapp/ui/weather/future/detail/FutureDetailWeatherViewModel.kt
+++ app/src/main/java/com/bensadiku/weatherapp/ui/weather/future/detail/FutureDetailWeatherViewModel.kt
@@ -1,7 +1,20 @@
 package com.bensadiku.weatherapp.ui.weather.future.detail
 
 import androidx.lifecycle.ViewModel;
+import com.bensadiku.weatherapp.data.provider.UnitProvider
+import com.bensadiku.weatherapp.data.repository.ForecastRepository
+import com.bensadiku.weatherapp.internal.lazyDeferred
+import com.bensadiku.weatherapp.ui.base.WeatherViewModel
+import org.threeten.bp.LocalDate
 
-class FutureDetailWeatherViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+class FutureDetailWeatherViewModel(
+    private val detailDate: LocalDate,
+    private val forecastRepository: ForecastRepository,
+    unitProvider: UnitProvider
+) : WeatherViewModel(forecastRepository, unitProvider) {
+
+    val weatherEntries by lazyDeferred {
+        forecastRepository.getFutureWeatherByDate(detailDate, super.isMetricUnit)
+    }
 }
