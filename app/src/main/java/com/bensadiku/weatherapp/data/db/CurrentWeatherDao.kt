d5e3f934e6a207b3530ece3a63e0aed2586c7ca8
bensadiku <43443348+bensadiku@users.noreply.github.com>
2019-02-06 02:07:44 +0100

    Added SuppressWarnings to be the getWeatherMetricQuery

--- app/src/main/java/com/bensadiku/weatherapp/data/db/CurrentWeatherDao.kt
+++ app/src/main/java/com/bensadiku/weatherapp/data/db/CurrentWeatherDao.kt
@@ -1,10 +1,7 @@
 package com.bensadiku.weatherapp.data.db
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.bensadiku.weatherapp.data.db.entity.CURRENT_WEATHER_ID
 import com.bensadiku.weatherapp.data.db.entity.CurrentWeatherEntry
 import com.bensadiku.weatherapp.data.db.unitlocalized.current.ImperialCurrentWeatherEntry
@@ -15,6 +12,7 @@ interface CurrentWeatherDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun upsert(weatherEntry: CurrentWeatherEntry)
 
+    @SuppressWarnings(RoomWarnings.CURSOR_MISMATCH)
     @Query("select * from current_weather where id =$CURRENT_WEATHER_ID")
     fun getWeatherMetric(): LiveData<MetricCurrentWeatherEntry>
 
